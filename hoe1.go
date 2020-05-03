bd410ef85a76334c66b0b77971f18aebd3fd5c96
Aakash Banerjee <aakash.banerjee@gmail.com>
2020-05-03 14:47:49 -0700

    hands on exercise - #1

---
+++ hoe1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	var y = "James Bond"
+	z := true
+
+	//single print statement
+	fmt.Printf("x: %v y: %v z: %v\n", x, y, z)
+	//multiple print statement
+	fmt.Println("x: ", x)
+	fmt.Println("y: ", y)
+	fmt.Println("z: ", z)
+}
